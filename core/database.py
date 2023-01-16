be12a6f1fb3607449f3694bb921fb1bf1c45fd5e
h3nryfost3r <henry.foster71@yahoo.com>
2023-01-16 17:22:12 +0300

    Updated env variables

--- core/database.py
+++ core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'postgresql://postgres:123456@localhost/resturant'
+SQLALCHAMY_DATABASE_URL = 'postgresql://postgres:123456@localhost:5433/resturant'
 engine = create_engine(SQLALCHAMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
